60cc3d635a46e1a3277692e6cab41416e412b015
lucas-giaimo <lucas.giaimo@laplateforme.io>
2024-11-07 09:55:12 +0100

    Grid function on terminal

--- Main.py
+++ Main.py
@@ -1,1 +1,29 @@
-Main
+grille=[" "," "," "," "," "," "," "," "," "]
+
+def case():                                               #grille avec support des variables dans le terminal
+ 
+    long = 1
+    larg = 1
+    n=0
+    global grille
+
+    print("+" + ("-" * long + "+") * 4)
+
+    for i in range(3):
+        
+        for k in range(3):
+            print(( f"|{grille[n]}|"),end='')
+            n+=1
+        print (end="\n") 
+
+    print("+" + ("-" * long + "+")*4)
+def turn(verif):
+    if verif == True:
+        symbol="X"
+    else:
+        symbol = "O" 
+
+        
+
+
+    
