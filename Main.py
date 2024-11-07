55e20ccec9439d60def453889e6f453daa1649d0
lucas-giaimo <lucas.giaimo@laplateforme.io>
2024-11-07 12:58:15 +0100

    victory correction and first version of solo tictactoe

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 grille=[" "," "," "," "," "," "," "," "," "]
-is_ended = False
+print_turn=0
+
 def case():                                               #grille avec support des variables dans le terminal
  
     long = 1
@@ -17,23 +18,60 @@ def case():                                               #grille avec support d
         print (end="\n") 
 
     print("+" + ("-" * long + "+")*4)
-
-
+          
+def turn_chooser():
+    global print_turn
+   
+    if print_turn == "O":
+        print_turn="X"      
+    else:
+        print_turn="O"
+          
 def victory():
     global grille
-    if grille[0] !=" " and grille [1] !=" " and grille [2] !=" " and grille [3] !=" " and grille [4] !=" " and grille [5] !=" " and grille[6]!=" " and grille [7] !=" " and grille [8] !=" ":
+    if " " not in grille:
         print("fin de partie, égalité")
-        return
-    elif grille[0] == grille[1] == grille[2] and grille[0]!=" " or grille[3] == grille[4] == grille[5] and grille[3]!= " " or grille[6] == grille[7] == grille[8] and grille[7]!=0:
+        return exit()
+    elif (grille[0] == grille[1] == grille[2] and grille[0] != " ") or (grille[3] == grille[4] == grille[5] and grille[3] != " ") or (grille[6] == grille[7] == grille[8] and grille[6] != " "):
         print(f"victoire en ligne horizontale ! GG")
-        return
-    elif grille[0] == grille[3] == grille[6] and grille[0] != " " or grille[1] == grille[4] == grille[7] and grille[4] !=" " or grille[2] == grille[5] == grille[8] and grille[2]!= 0:
-        print(f"victoire en ligne vertical ! GG")
-        return
-    elif grille[0] == grille[4] == grille[8] and grille[0]!=" " or grille[2] == grille[4] == grille[6] and grille[4] != " ":
+        return exit()
+    elif (grille[0] == grille[3] == grille[6] and grille[0] != " ") or (grille[1] == grille[4] == grille[7] and grille[1] != " ") or (grille[2] == grille[5] == grille[8] and grille[2] != " "):
+        print(f"victoire en ligne verticale ! GG")
+        return exit()
+    elif (grille[0] == grille[4] == grille[8] and grille[0] != " ") or (grille[2] == grille[4] == grille[6] and grille[2] != " "):
         print("victoire en diagonale ! GG")
-        return
-victory()
+        return exit()
+    else:
+        print("manche suivante")
+
+def choose():       #demande la case qu'il souhaite
+    global grille
+    global print_turn
+    select=(int(input("Joueur 1 choisissez une case")))
+    if grille[select-1] ==" ":
+        grille[select] = print_turn
+    else: 
+        print("choisissez une case vide")
+        case()
+        choose()
+
+def turn():
+    global print_turn
+    global grille
+    turn_chooser() #change turn each times
+    case() #display
+    choose()
+        
+    case() #display
+    victory() #check victory
+    turn()
+turn()
+    
+    
+        
+    
+        
+
          
         
         
