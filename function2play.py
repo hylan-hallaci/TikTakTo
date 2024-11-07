2f9196ab8f12b4194f010b03088f68f15b7cb700
jeffry-khalife <jeffry.khalife@laplateforme.io>
2024-11-07 13:27:14 +0100

    update

--- function2play.py
+++ function2play.py
@@ -1,4 +1,5 @@
-def turn_chose():
+print_turn=0
+def turn_chose(): # fonction qui permet d'alterner entre le Joueur X et le joueur O
      global grille
      global print_turn
      if print_turn == "O":
@@ -6,21 +7,24 @@ def turn_chose():
      else:
           print_turn = "O"
 
-def turn():
-    global grille
-    turn_chose()
-    case()
-    turn_chose()
-    case()
-       
 
-def two_players():
+def two_players():      #fonction qui demande a l'utilisateur la case qu'il souhaite
     global grille
     global print_turn
     new_value = int(input("Choisir la case: "))
     if grille[new_value] == " ":
             grille[new_value]= print_turn
-    else:
-            print("choisissez une case vide svp")
-    victory()
-    
+    else: 
+        print("choisissez une case vide")
+        case()
+        two_players()
+
+def turn():
+    global grille
+    case()    #display de la grille 
+    turn_chose() # changement de joueur
+    two_players() # demande la case à jouer  
+    case()
+    victory() # verifie la victoire 
+    turn()
+turn()
